b44eb9e985255e8d3aee9622b16729083c6d0358
Huntsman-Imran <helloimran24h@gmail.com>
2016-09-12 01:35:06 +0600

    A templet designed from PSD
    It's  a sample templet of a IT company whch is designed from PSD file

---
+++ index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Workshop</title>
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link type="text/css" rel="stylesheet" href="css/font-awesome.min.css"/>
+    <link type="text/css" rel="stylesheet" href="main.css"/>
+</head>
+<body>
+    
+    <div class="jumbotron">
+    	<div class="container">
+
+            <!--nav bar start-->
+    		<nav class="navbar navbar-default">
+                <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                    <a href="#" class="navbar-brand">Brand</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        			<ul class="nav navbar-nav navbar-right">
+        				<li class="active"><a href="#">Home</a></li>
+        				<li><a href-"#">About Us</a></li>
+        				<li><a href-"#">Blog</a></li>
+        				<li><a href-"#">Services</a></li>
+        				<li><a href-"#">Contuct Us</a></li>
+        			</ul>
+                </div>
+    		</nav>
+            <!--nav bar end-->
+
+            <!--banner start-->
+            
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                           <div class="respons text-center">
+                                <h1>We Build Well Designed Web Site</h1>
+                                <h4>The Website is all about well website design</h4>
+
+                                <a class="btn btn-primary" href="#">Get Started</a>
+                                <a href="#">Learn more</a>
+                                 <img class="center-block img-responsive" src="img/browser.png" alt="">
+                            </div>
+                        </div>
+                        <div class="item">
+                           <div class="respons text-center">
+                                <h1>We Build Well Designed Web Site</h1>
+                                <h4>The Website is all about well website design</h4>
+
+                                <a class="btn btn-primary" href="#">Get Started</a>
+                                <a href="#">Learn more</a>
+                                 <img class="center-block img-responsive" src="img/browser.png" alt="">
+                            </div>
+                    
+                        </div>
+                        <div class="item">
+                           <div class="respons text-center">
+                                <h1>We Build Well Designed Web Site</h1>
+                                <h4>The Website is all about well website design</h4>
+
+                                <a class="btn btn-primary" href="#">Get Started</a>
+                                <a href="#">Learn more</a>
+                                 <img class="center-block img-responsive" src="img/browser.png" alt="">
+                            </div>
+                    
+                        </div>
+                      </div>
+
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+            <!--banner end-->
+
+    	</div>
+    </div>
+
+    <!--process start-->
+    <div class="process">
+        <div class="container">
+            <div class="row">
+                <div class="process-sprit-line hidden-xs hidden-sm">&nbsp;</div>
+                <div class="col-md-3">
+    
+                        <div class="plane text-center">
+                            <span class="pocss-icon iplane">&nbsp;</span>
+                            <h3 class="heading">Plane</h3>
+                            <p>
+                                developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and 
+                                web developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and web development
+                            </p>
+                            <a class="btn btn-default" href="#">Read More</a>
+                        </div>
+                </div>
+                 <div class="col-md-3">
+    
+                    <div class="design text-center">
+                        <span class="pocss-icon idesign">&nbsp;</span>
+                        <h3 class="heading">Design</h3>
+                        <p>
+                            developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and web 
+                            developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and web development
+                        </p>
+                        <a class="btn btn-default" href="#">Read More</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+    
+                           <div class="develop text-center">
+                            <span class="pocss-icon idevelop">&nbsp;</span>
+                            <h3 class="heading">Develop</h3>
+                            <p>
+                                developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and web developmentHello
+                                 every one tthid one of the most wanderful site where you can learn eveything about the web design and web development
+                            </p>
+                            <a class="btn btn-default" href="#">Read More</a>
+                           </div>
+    
+                </div>
+                <div class="col-md-3">
+    
+                            <div class="lounch text-center">
+                                <span class="pocss-icon ilounch">&nbsp;</span>
+                                <h3 class="heading">Lounch</h3>
+                                <p>
+                                    developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and web developmentHello
+                                     every one tthid one of the most wanderful site where you can learn eveything about the web design and web development
+                                </p>
+                                <a class="btn btn-default" href="#">Read More</a>
+                            </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--process start-->
+
+    <!--portfolio start-->
+    <div class="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">    
+                        <div class="text-left">
+                            <h3 class="heading">Portfolio</h3>
+                            <p>
+                                developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and 
+                                web developmentHello every one tthid one of the most wanderful site where you can learn eveything about the web design and web development
+                            </p>
+                            <a href="#">Our Portfolio</a>
+                        </div>
+                </div>
+                 <div class="col-md-3">    
+                    <div class="text-center">
+                    
+                        <img class="img-thumbnail" src="img/thumbnail.png" alt="portfolio">
+
+                        <h4 class="portfolio-heading">Best Web Designer</h4>
+                        <small class="category">Web Design</small>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                           <div class="text-center">
+                        
+                            <img class="img-thumbnail" src="img/thumbnail-1.png" alt="portfolio">
+    
+                            <h4 class="portfolio-heading">Portfolio</h4>
+                            <small class="category">Web Development</small>
+                           </div>
+    
+                </div>
+                <div class="col-md-3">
+                   <div class="text-center">
+                    <img class="img-thumbnail" src="img/thumbnail-2.png" alt="portfolio">
+                    <h4 class="portfolio-heading">eCommerce</h4>
+                    <small class="category">Web Development</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--portfolio end-->
+
+    <!--partner start-->
+     <div class="partner">
+        <div class="container">
+          <div class="row">
+             <div class="col-md-3">
+            <img class="p-item center-block img-responsive" src="img/partnar-1.png" alt="partnar">
+        </div>
+              <div class="col-md-3">
+            <img class="p-item center-block img-responsive" src="img/partnar-2.png" alt="partnar">
+        </div>
+              <div class="col-md-3">
+            <img class="p-item center-block img-responsive" src="img/partnar-3.png" alt="partnar">
+        </div>
+              <div class="col-md-3">
+            <img class="p-item center-block img-responsive" src="img/partnar-4.png" alt="partnar">
+        </div>
+          </div>
+     </div>
+ </div>
+    <!--partner end-->
+
+    <!--widgets start-->
+    <div class="widgets">
+        <div class="container">
+          <div class="row">
+             <div class="col-md-4">
+                <div class="testimonial">
+                    <h3 class="heading">Testimonial</h3> 
+                       <div id="testimonialcarousel" class="carouselTestimonial slide" data-ride="carousel">
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+             <img alt="testimonil" class="pull-left" src="img/testimonil.png"> 
+                         <blockquote>
+                            <p>development Hello Everyonre this is one of the most wonderful site where you
+                             can leart everything about websitedevelopment Hello Everyonre this
+                             is one of the most wonderful site where you can leart everything about website</p>
+                             <cite>
+                                <strong>9Gag</strong><br/>
+                                <a href="#">www.9gag.com</a>
+                             </cite>
+                         </blockquote>
+                          </div>
+                           <div class="item">
+             <img alt="testimonil" class="pull-left" src="img/testimonil.png"> 
+                         <blockquote>
+                            <p>development Hello Everyonre this is one of the most wonderful site where you
+                             can leart everything about websitedevelopment Hello Everyonre this
+                             is one of the most wonderful site where you can leart everything about website</p>
+                             <cite>
+                                <strong>9Gag</strong><br/>
+                                <a href="#">www.9gag.com</a>
+                             </cite>
+                         </blockquote>
+                          </div>
+                           <div class="item">
+             <img alt="testimonil" class="pull-left" src="img/testimonil.png"> 
+                         <blockquote>
+                            <p>development Hello Everyonre this is one of the most wonderful site where you
+                             can leart everything about websitedevelopment Hello Everyonre this
+                             is one of the most wonderful site where you can leart everything about website</p>
+                             <cite>
+                                <strong>9Gag</strong><br/>
+                                <a href="#">www.9gag.com</a>
+                             </cite>
+                         </blockquote>
+                          </div>
+                        </div>
+                         </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="blog">
+                <h3 class="heading">Our Latest Post</h3>
+                <ul class="list-unstyled blog-list">
+                    <li>
+                        <h5><a href="#">This is photoshop version of loram ipsam</a></h5>
+                        <small>Jun 4,2013 | Posted by <a href="#">Adimin</a></small>
+                    </li>
+                    <li>
+                        <h5><a href="#">This is photoshop version of loram ipsam</a></h5>
+                        <small>Jun 4,2013 | Posted by <a href="#">Adimin</a></small>
+                    </li>
+                    <li>
+                        <h5><a href="#">This is photoshop version of loram ipsam</a></h5>
+                        <small>Jun 4,2013 | Posted by <a href="#">Adimin</a></small>
+                    </li>
+                </ul>
+                 </div>
+            </div>
+            <div class="col-md-4">
+                <div class="tweet">
+                <h3 class="heading">Latest Tweets</h3>
+                 <div class="tweet-list">
+                    <ul class="list-unstyled tweet-list">
+                    <li class="clearfix">
+                        <img  class="pull-left" src="img/small-thumb.png" alt="small-thumb">
+                      <h5>This is photoshop version of loram ipsam</h5>
+                        <small><a href="#">http://www.helloimran24h@gmail.com</a></small>
+                    </li>
+                    <li  class="clearfix">
+                        <img  class="pull-left"src="img/small-thumb.png" alt="small-thumb">
+                      <h5>This is photoshop version of loram ipsam</h5>
+                        <small><a href="#">http://www.helloimran24h@gmail.com</a></small>
+                    </li>
+                    <li  class="clearfix">
+                        <img class="pull-left" src="img/small-thumb.png" alt="small-thumb">
+                        <h5>This is photoshop version of loram ipsam</h5>
+                        <small><a href="#">http://www.helloimran24h@gmail.com</a></small>
+                    </li>
+                </ul>
+                 </div>
+               </div>
+            </div>
+          </div>
+        </div>
+    </div>
+     <!--widgets start-->
+
+       <!--footer start-->
+       <div class="footer">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-12 text-center">
+                <ul class="list-unstyled list-inline">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+                 
+                 <div class="social">
+                     <a href="#" class="btn btn-primary facebook"><span class="social-sprit facebook">&nbsp;</span>Follow Us On Facebook</a>
+                     <a href="#" class="btn btn-primary twitter"><span class="social-sprit twitter">&nbsp;</span>Follow Us On Twitter</a>
+                 </div>
+
+                 <small>&copy;<strong><a href="#">IMRAN</a></strong>2016</small>
+
+            </div>
+        </div>
+     </div>
+       </div>   
+       <!--footer end-->
+
+
+	    <script src="js/jquery-1.11.3.min.js"></script>
+       <script src="js/bootstrap.min.js"></script>
+       <script src="js/jquery-ui.js"></script>
+       <script>
+       $(function(){
+         $('.carousel').carousel();
+         $('.carouselTestimonial').carousel();
+       });
+       </script>
+
+</body>
+</html>
